7cd52eaa6f1e53db57ad9024b9c0792a16121352
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-23 16:54:50 +0600

    navbar added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../components/shared/navbar/Navbar';
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet/>
         </div>
     );
--- src/pages/home/HomeBanner/HomeBanner.jsx
+++ src/pages/home/HomeBanner/HomeBanner.jsx
@@ -5,17 +5,17 @@ import BtnPrimary from "../../../components/btn/BtnPrimary";
 const HomeBanner = () => {
   return (
     <div
-      className="min-h-[60vh] md:h-screen w-screen bg-no-repeat bg-center md:bg-cover "
+      className="min-h-[60vh] md:h-screen w-full bg-no-repeat bg-center md:bg-cover "
       style={{ backgroundImage: `url(${sliderImg})` }}
     >
-      <div className="flex items-center justify-center w-full h-full" style={{backgroundColor:' transparent',
+      <div className="flex items-center justify-center w-full min-h-[60vh] h-full" style={{backgroundColor:' transparent',
     backgroundImage: 'linear-gradient(180deg, #00000073 47%, #000000 100%)',
     opacity: '0.86'}}> 
-        <div className="text-center">
-          <h1 className="text-xl md:text-3xl lg:text-7xl text-white font-extrabold font-mono">
+        <div className="text-center px-3 lg:px-0">
+          <h1 className="text-3xl lg:text-7xl text-white font-extrabold font-mono">
             Poultry Farming
           </h1>
-          <p className="text-[#EDECD8] font-medium text-sm lg:text-base py-4 md:py-6 max-w-[651px]">
+          <p className="text-[#EDECD8] font-medium text-xs md:text-sm lg:text-base py-4 md:py-6 max-w-[651px]">
             All of our chickens are grown by the Soanes family and their farming
             neighbours on farms in and around the Yorkshire Wolds.
           </p>
