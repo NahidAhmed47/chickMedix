598e1be3f12267b8597cfd4fdf0a094766915b47
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-23 20:06:27 +0600

    team card section added

--- src/pages/home/home/Home.jsx
+++ src/pages/home/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HomeBanner from '../HomeBanner/HomeBanner';
 import QualityAndCards from '../qualityAndCards/QualityAndCards';
+import OurTeam from '../ourTeam/OurTeam';
 
 const Home = () => {
     return (
         <div>
             <HomeBanner/>
             <QualityAndCards></QualityAndCards>
+            <OurTeam></OurTeam>
         </div>
     );
 };
