e585b6dc66f1ecdb9ccef19821718ec657a36286
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-23 17:55:13 +0600

    footer added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/shared/navbar/Navbar';
+import Footer from '../components/shared/navbar/footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet/>
+            <Footer></Footer>
         </div>
     );
 };
